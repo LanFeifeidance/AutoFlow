5a854c0455c3625d66496e290bd0003cb0e08af0
Your Name <1437701092@qq.com>
2017-09-15 20:13:32 +0800

    自定义GridLayout

---
+++ AutoFLowLayout.java
@@ -0,0 +1,24 @@
+package jiyun.ytp.com.autoflow;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.GridLayout;
+
+/**
+ * 生命不息，奋斗不止，万事起于细微，量变引起质变
+ * Created by 因天鹏 on 2017/9/15.
+ */
+
+public class AutoFLowLayout extends GridLayout{
+    public AutoFLowLayout(Context context) {
+        super(context);
+    }
+
+    public AutoFLowLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AutoFLowLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
